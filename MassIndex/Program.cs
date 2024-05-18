6ed044d68c864d0117097f63ad5067eaa1a02e84
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-05-18 07:13:01 +0400

    Add files via upload

---
+++ MassIndex/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mass index = kilo/boy*boy
+            //18 ve ya daha asagi ZEIF
+            //18 ve 25 arasi NORMAL
+            //25den yuxari OBEZ
+            Console.Write("Kilonuzu yazin : ");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Boyunuzu yazin : ");
+            double boy = Convert.ToDouble(Console.ReadLine());
+            
+
+            double massindex = kilo / (boy * boy);
+            Console.WriteLine("Your mass index : " + massindex);
+
+            if (massindex == 18 || massindex <= 18)
+            {
+                Console.WriteLine("ZEIF!");
+            }
+            else if (massindex > 18 && massindex <= 25)
+            {
+                Console.WriteLine("NORMAL!");
+            }
+            else 
+            {
+                Console.WriteLine("OBEZ!");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
