3d8c1be6c5db6a474e3fcef2a16e312b1a5f87b9
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-05-19 19:25:46 +0400

    Add files via upload

---
+++ BirthDayProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the current year : ");
+            int cyear = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int birthday = cyear - age;
+            Console.WriteLine("User's birthday : " + birthday);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
